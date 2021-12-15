8b2d80b721aead4fb0e9811762ec08a6e3a0e2ce
Ihor Mandzyuk <ihor.mandzyuk@magefan.com>
2021-12-15 15:54:22 +0200

    add rewrited files

---
+++ peregrine/lib/talons/RootComponents/Category/categoryFragments.gql.js
@@ -0,0 +1,43 @@
+import { gql } from '@apollo/client';
+
+export const CategoryFragment = gql`
+    fragment CategoryFragment on CategoryTree {
+        id
+        meta_title
+        meta_keywords
+        meta_description
+        mf_alternate_urls {
+            href
+            hreflang
+        }
+    }
+`;
+
+export const ProductsFragment = gql`
+    fragment ProductsFragment on Products {
+        items {
+            id
+            uid
+            name
+            price_range {
+                maximum_price {
+                    regular_price {
+                        currency
+                        value
+                    }
+                }
+            }
+            sku
+            small_image {
+                url
+            }
+            stock_status
+            type_id
+            url_key
+        }
+        page_info {
+            total_pages
+        }
+        total_count
+    }
+`;
